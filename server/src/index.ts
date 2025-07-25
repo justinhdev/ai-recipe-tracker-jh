d0f6e1fb16bfbd11237fdb3f173aff38d16b85c0
Justin Hancock <jhancdev@gmail.com>
2025-07-25 10:00:45 -0500

    Add ESLint and Prettier setup; format existing codebase

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.send('Server is running!');
+app.get("/", (_req, res) => {
+  res.send("Server is running!");
 });
 
 const PORT = process.env.PORT || 3000;
